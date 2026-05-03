2eeaacaf1b9b73ee7e8e134ab9d74b80d5fb2848
Your Name <you@example.com>
2026-05-03 00:54:47 -0500

    fix postcss tailwind v4 configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// postcss.config.js
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {}, // ប្តូរពី 'tailwindcss' មកជារបស់ថ្មីនេះ
+    "autoprefixer": {},
   },
 }
