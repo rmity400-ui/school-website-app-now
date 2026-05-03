49dadf74066a9a37a6a272a42cd759284263a902
Your Name <you@example.com>
2026-05-03 05:41:02 -0500

    Fix: properly define app and firebase services

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { app } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSy...",
