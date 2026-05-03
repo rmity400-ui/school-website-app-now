e042229f02ff6984bcf2efa3ff8d1a21b9692845
Your Name <you@example.com>
2026-05-03 08:24:16 -0500

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 
-
+import { app } from './firebaseConfig'; //
 const firebaseConfig = {
   apiKey: "AIzaSyBGExtb5ZiemY3wWpWj_fbp5rp6BbaDbcc",
   authDomain: "school-app-36954.firebaseapp.com",
@@ -10,3 +10,4 @@ const firebaseConfig = {
   appId: "1:449537720077:web:e206b310294713276a7a44",
   measurementId: "G-BBCLLG82HD"
 };
+const app = initializeApp(firebaseConfig)
