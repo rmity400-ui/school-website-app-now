13c903ad866ce0d9824a7760906fd3fffbd08e46
Your Name <you@example.com>
2026-05-03 08:57:37 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // 1. React Hooks
 import React, { useState, useEffect, useRef } from 'react';
-import { auth, db } from "./firebase/firebaseConfig";
+import { auth, db } from "./firebase";
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, signInAnonymously, signInWithCustomToken } from 'firebase/auth';
 
