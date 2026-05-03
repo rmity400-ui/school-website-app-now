3f6f1a27460950dabfd7e000ca8997f056c1f1bd
Your Name <you@example.com>
2026-05-03 05:37:38 -0500

    update project

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBGExtb5ZiemY3wWpWj_fbp5rp6BbaDbcc",
+  apiKey: "AIzaSy...",
   authDomain: "school-app-36954.firebaseapp.com",
-  databaseURL: "https://school-app-36954-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "school-app-36954",
   storageBucket: "school-app-36954.firebasestorage.app",
   messagingSenderId: "449537720077",
-  appId: "1:449537720077:web:e206b310294713276a7a44",
-  measurementId: "G-BBCLLG82HD"
+  appId: "1:449537720077:web:e206b310294713276a7a44"
 };
 
-// Services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
 
-// Export
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db }; // ✅ មិន export app
