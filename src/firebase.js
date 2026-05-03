49b839d6219959da9ae6837743202967fe616ad8
Your Name <you@example.com>
2026-05-03 05:44:37 -0500

    update project

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyBGExtb5ZiemY3wWpWj_fbp5rp6BbaDbcc",
   authDomain: "school-app-36954.firebaseapp.com",
+  databaseURL: "https://school-app-36954-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "school-app-36954",
   storageBucket: "school-app-36954.firebasestorage.app",
   messagingSenderId: "449537720077",
-  appId: "1:449537720077:web:e206b310294713276a7a44"
+  appId: "1:449537720077:web:e206b310294713276a7a44",
+  measurementId: "G-BBCLLG82HD"
 };
 
 const app = initializeApp(firebaseConfig); 
