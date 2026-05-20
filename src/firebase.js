85236b16084c95ae5f63cf881f2a637eeb0bb0d9
school-website-app-now <rmity400@gmail.com>
2026-05-20 22:40:29 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,12 @@
-
-// ក្នុង file firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGExtb5ZiemY3wWpWj_fbp5rp6BbaDbcc",
+  apiKey: "PASTE_NEW_KEY_HERE",
   authDomain: "school-app-36954.firebaseapp.com",
-  databaseURL: "https://school-app-36954-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "school-app-36954",
-  storageBucket: "school-app-36954.firebasestorage.app",
+  storageBucket: "school-app-36954.appspot.com",
   messagingSenderId: "449537720077",
-  appId: "1:449537720077:web:e206b310294713276a7a44",
-  measurementId: "G-BBCLLG82HD"
+  appId: "1:449537720077:web:e206b310294713276a7a44"
 };
 
-
-export const auth = getAuth(app); // ត្រូវតែមានពាក្យ export
-export const db = getFirestore(app); // ត្រូវតែមានពាក្យ export
+export const app = initializeApp(firebaseConfig);
