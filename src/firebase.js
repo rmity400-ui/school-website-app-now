3e88fc4679a18d20b9ef50fae1d7b110a4874a47
school-website-app-now <rmity400@gmail.com>
2026-05-20 22:24:26 +0700

    upate porjet

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// ក្នុង file firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBGExtb5ZiemY3wWpWj_fbp5rp6BbaDbcc",
   authDomain: "school-app-36954.firebaseapp.com",
@@ -17,6 +15,6 @@ const firebaseConfig = {
   measurementId: "G-BBCLLG82HD"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app); // ត្រូវតែមានពាក្យ export
+export const db = getFirestore(app); // ត្រូវតែមានពាក្យ export
