c12cbeedd6d7b9ad0825b2c2f9c6bee1cdfab5a6
school-website-app-now <rmity400@gmail.com>
2026-05-20 22:15:07 +0700

    update add key firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,11 @@
-
-// ក្នុង file firebaseConfig.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBGExtb5ZiemY3wWpWj_fbp5rp6BbaDbcc",
   authDomain: "school-app-36954.firebaseapp.com",
@@ -15,7 +17,6 @@ const firebaseConfig = {
   measurementId: "G-BBCLLG82HD"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app); // ត្រូវតែមានពាក្យ export
-export const db = getFirestore(app); // ត្រូវតែមានពាក្យ export
+const analytics = getAnalytics(app);
