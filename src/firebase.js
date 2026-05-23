3572ffa5a5b74cca382700a500626a8d2621f9df
school-website-app-now <rmity400@gmail.com>
2026-05-23 22:25:20 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,22 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBGExtb5ZiemY3wWpWj_fbp5rp6BbaDbcc",
   authDomain: "school-app-36954.firebaseapp.com",
   databaseURL: "https://school-app-36954-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "school-app-36954",
-  storageBucket: "school-app-36954.firebasestorage.app",
+  storageBucket: "school-app-36954.appspot.com",
   messagingSenderId: "449537720077",
   appId: "1:449537720077:web:e206b310294713276a7a44",
+  measurementId: "G-BBCLLG82HD"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+const analytics = getAnalytics(app);
+
+export default app;
